50213893bd0bb245137b84c2669dc619ba8de42e
Darragh <darragh1992@gmail.com>
2016-04-08 12:21:50 +0100

    First Commit

---
+++ accounts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.utils import timezone
+
+
+# Create your models here.
+
+class AccountUserManager(UserManager):
+    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given username must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(username=email, email=email, is_staff=is_staff, is_active=True, is_superuser=is_superuser,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractUser):
+    objects = AccountUserManager()
